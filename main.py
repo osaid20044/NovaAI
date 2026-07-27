42098633d93b9b2580c47fdb638f9b6f561e96b9
osaid <yasin2000osid@gmail.com>
2026-07-28 02:02:14 +0300

    added basic NovaAI converion

---
+++ main.py
@@ -0,0 +1,11 @@
+name = input ("what is you name")
+print ("hello"  + name + "my name NovaAI" )
+question = input ("how can i help you")
+if "hello" in question:
+     print ("hello"+ name )
+elif "python" in question:
+    print ("python is great a languge")
+elif "bye" in question:
+    print("godbye" + name )
+else :
+    print("i dont umderstand you")            
